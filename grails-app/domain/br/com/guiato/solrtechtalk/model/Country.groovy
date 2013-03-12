507db5ca37b02db4004c047aae2bfe925697ace0
Rodrigo Peleias <rodrigo.peleias@guiato.com.br>
2013-03-12 15:29:39 -0300

    Add domain class Properties and Solr configuration

--- grails-app/domain/br/com/guiato/solrtechtalk/model/Country.groovy
+++ grails-app/domain/br/com/guiato/solrtechtalk/model/Country.groovy
@@ -2,6 +2,14 @@ package br.com.guiato.solrtechtalk.model
 
 class Country {
 
-    static constraints = {
-    }
+	// enable indexing of a domain class. This will enable the dynamic methods on the domain object.
+	static enableSorSearch = true
+
+	//To have the index updated on inserts, updates and deletes of your domain classes
+	static solrAutoIndex = true
+
+    static hasMany = [states: State]
+
+    String name
+    String acronym
 }
--- grails-app/domain/br/com/guiato/solrtechtalk/model/State.groovy
+++ grails-app/domain/br/com/guiato/solrtechtalk/model/State.groovy
@@ -2,6 +2,15 @@ package br.com.guiato.solrtechtalk.model
 
 class State {
 
-    static constraints = {
-    }
+	// enable indexing of a domain class. This will enable the dynamic methods on the domain object.
+	static enableSorSearch = true
+
+	//To have the index updated on inserts, updates and deletes of your domain classes
+	static solrAutoIndex = true
+	
+	static hasOne = [country: Country] 
+    static hasMany = [cities: City]
+
+    String name
+    String uf
 }
