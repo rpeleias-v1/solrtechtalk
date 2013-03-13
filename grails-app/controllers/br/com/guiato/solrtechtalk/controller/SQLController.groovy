4a36c6bcae7dba4aede8e2bc7c91cf48bae30344
Rodrigo Peleias <Rodrigo Peleias>
2013-03-12 22:21:20 -0300

    Add methos to SQLController

--- grails-app/controllers/br/com/guiato/solrtechtalk/controller/SQLController.groovy
+++ grails-app/controllers/br/com/guiato/solrtechtalk/controller/SQLController.groovy
@@ -7,14 +7,30 @@ class SQLController {
 	def sqlService
 
     def findAllCoutries = {
-    
+    	def model = [
+    		countries: sqlService.findAllCoutries();
+    	]
+    	render(model: model)
     }
 
     def findStatesByCountry = {
-
+    	def selectedCountry = params?.country
+    	if (selectedCountry) {
+    		def model = [
+    			states: sqlService.findStatesByCountry(selectedCountry);
+    		]
+    		render(model: model)
+    	}
     }
 
     def listCitiesByCountryAndState = {
-    	
+    	def selectedCountry = params?.country
+    	def selectedState = params?.state
+    	if (selectedCountry && selectedState) {
+    		def model = [
+    			cities: sqlService.listCitiesByCountryAndState(selectedCountry, selectedState)
+    		]
+    	}
+    	render(model: model)
     }
 }
